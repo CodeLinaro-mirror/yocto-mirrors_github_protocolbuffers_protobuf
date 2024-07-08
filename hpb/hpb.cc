mamh-mixed/protobuf

superficial file rename protos_* -> hpb_*
PiperOrigin-RevId: 649157361

hpb/hpb.cc
@@ -5,7 +5,7 @@
 // license that can be found in the LICENSE file or at
 // https://developers.google.com/open-source/licenses/bsd
 
-#include "google/protobuf/hpb/protos.h"
+#include "google/protobuf/hpb/hpb.h"
 
 #include <atomic>
 #include <cstddef>
@@ -14,7 +14,7 @@
 #include "absl/status/statusor.h"
 #include "absl/strings/str_format.h"
 #include "absl/strings/string_view.h"
-#include "google/protobuf/hpb/protos_extension_lock.h"
+#include "google/protobuf/hpb/hpb_extension_lock.h"
 #include "upb/mem/arena.h"
 #include "upb/message/accessors.h"
 #include "upb/message/copy.h"

hpb/repeated_field.h
@@ -15,8 +15,8 @@
 #include <type_traits>
 
 #include "absl/strings/string_view.h"
-#include "google/protobuf/hpb/protos.h"
-#include "google/protobuf/hpb/protos_traits.h"
+#include "google/protobuf/hpb/hpb.h"
+#include "google/protobuf/hpb/hpb_traits.h"
 #include "google/protobuf/hpb/repeated_field_iterator.h"
 #include "upb/base/string_view.h"
 #include "upb/mem/arena.h"

protos/protos_extension_lock.h
@@ -7,5 +7,5 @@
 
 #ifndef UPB_PROTOS_PROTOS_EXTENSION_LOCK_H_
 #define UPB_PROTOS_PROTOS_EXTENSION_LOCK_H_
-#include "google/protobuf/hpb/protos_extension_lock.h"
+#include "google/protobuf/hpb/hpb_extension_lock.h"
 #endif

protos/protos_internal.h
@@ -7,5 +7,5 @@
 
 #ifndef UPB_PROTOS_PROTOS_INTERNAL_H_
 #define UPB_PROTOS_PROTOS_INTERNAL_H_
-#include "google/protobuf/hpb/protos_internal.h"
+#include "google/protobuf/hpb/hpb_internal.h"
 #endif

protos/protos_traits.h
@@ -7,5 +7,5 @@
 
 #ifndef UPB_PROTOS_PROTOS_TRAITS_H_
 #define UPB_PROTOS_PROTOS_TRAITS_H_
-#include "google/protobuf/hpb/protos_traits.h"
+#include "google/protobuf/hpb/hpb_traits.h"
 #endif

CodeLinaro-mirror/yocto-mirrors_github_protocolbuffers_protobuf

hpb/hpb.cc
@@ -5,7 +5,7 @@
 // license that can be found in the LICENSE file or at
 // https://developers.google.com/open-source/licenses/bsd
 
-#include "google/protobuf/hpb/protos.h"
+#include "google/protobuf/hpb/hpb.h"
 
 #include <atomic>
 #include <cstddef>
@@ -14,7 +14,7 @@
 #include "absl/status/statusor.h"
 #include "absl/strings/str_format.h"
 #include "absl/strings/string_view.h"
-#include "google/protobuf/hpb/protos_extension_lock.h"
+#include "google/protobuf/hpb/extension_lock.h"
 #include "upb/mem/arena.h"
 #include "upb/message/accessors.h"
 #include "upb/message/copy.h"

hpb/repeated_field.h
@@ -15,9 +15,9 @@
 #include <type_traits>
 
 #include "absl/strings/string_view.h"
-#include "google/protobuf/hpb/protos.h"
-#include "google/protobuf/hpb/protos_traits.h"
+#include "google/protobuf/hpb/hpb.h"
 #include "google/protobuf/hpb/repeated_field_iterator.h"
+#include "google/protobuf/hpb/traits.h"
 #include "upb/base/string_view.h"
 #include "upb/mem/arena.h"
 #include "upb/message/array.h"

protos/protos_extension_lock.h
@@ -7,5 +7,5 @@
 
 #ifndef UPB_PROTOS_PROTOS_EXTENSION_LOCK_H_
 #define UPB_PROTOS_PROTOS_EXTENSION_LOCK_H_
-#include "google/protobuf/hpb/protos_extension_lock.h"
+#include "google/protobuf/hpb/extension_lock.h"
 #endif

protos/protos_internal.h
@@ -7,5 +7,5 @@
 
 #ifndef UPB_PROTOS_PROTOS_INTERNAL_H_
 #define UPB_PROTOS_PROTOS_INTERNAL_H_
-#include "google/protobuf/hpb/protos_internal.h"
+#include "google/protobuf/hpb/internal.h"
 #endif

protos/protos_traits.h
@@ -7,5 +7,5 @@
 
 #ifndef UPB_PROTOS_PROTOS_TRAITS_H_
 #define UPB_PROTOS_PROTOS_TRAITS_H_
-#include "google/protobuf/hpb/protos_traits.h"
+#include "google/protobuf/hpb/traits.h"
 #endif