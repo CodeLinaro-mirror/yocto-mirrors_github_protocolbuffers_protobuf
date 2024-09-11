reihoyo/protobuf

Migrate UpbStrToStringView from hpb to hpb::interop::upb
PiperOrigin-RevId: 672958997

hpb/backend/upb/interop.h
@@ -10,8 +10,10 @@
 
 // The sole public header in hpb/backend/upb
 
+#include "absl/strings/string_view.h"
 #include "google/protobuf/hpb/internal/internal.h"
 #include "google/protobuf/hpb/ptr.h"
+#include "upb/base/string_view.h"
 #include "upb/mem/arena.h"
 #include "upb/message/message.h"
 #include "upb/mini_table/message.h"
@@ -77,6 +79,10 @@ typename T::CProxy MakeCHandle(const upb_Message* msg, upb_Arena* arena) {
 template <typename T>
 typename T::Proxy MakeHandle(upb_Message* msg, upb_Arena* arena) {
   return typename T::Proxy(msg, arena);
+}
+
+inline absl::string_view UpbStrToStringView(upb_StringView str) {
+  return absl::string_view(str.data, str.size);
 }
 
 }  // namespace hpb::interop::upb

hpb_generator/gen_accessors.cc
@@ -225,7 +225,7 @@ void WriteAccessorsInSource(const protobuf::Descriptor* desc, Output& output) {
         output(
             R"cc(
               $1 $0::$2() const {
-                return ::hpb::UpbStrToStringView($3_$4(msg_));
+                return hpb::interop::upb::UpbStrToStringView($3_$4(msg_));
               }
             )cc",
             class_name, CppConstType(field), resolved_field_name,

hpb_generator/gen_repeated_fields.cc
@@ -204,7 +204,7 @@ void WriteRepeatedStringAccessor(const protobuf::Descriptor* message,
           size_t len;
           auto* ptr = $3_mutable_$4(msg_, &len);
           assert(index < len);
-          return ::hpb::UpbStrToStringView(*(ptr + index));
+          return hpb::interop::upb::UpbStrToStringView(*(ptr + index));
         }
       )cc",
       class_name, CppConstType(field), resolved_field_name,

CodeLinaro-mirror/yocto-mirrors_github_protocolbuffers_protobuf

hpb/backend/upb/interop.h
@@ -10,8 +10,10 @@
 
 // The sole public header in hpb/backend/upb
 
+#include "absl/strings/string_view.h"
 #include "google/protobuf/hpb/internal/internal.h"
 #include "google/protobuf/hpb/ptr.h"
+#include "upb/base/string_view.h"
 #include "upb/mem/arena.h"
 #include "upb/message/message.h"
 #include "upb/mini_table/message.h"
@@ -91,6 +93,10 @@ typename T::CProxy MakeCHandle(const upb_Message* msg, upb_Arena* arena) {
 template <typename T>
 typename T::Proxy MakeHandle(upb_Message* msg, upb_Arena* arena) {
   return typename T::Proxy(msg, arena);
+}
+
+inline absl::string_view FromUpbStringView(upb_StringView str) {
+  return absl::string_view(str.data, str.size);
 }
 
 }  // namespace hpb::interop::upb

hpb_generator/gen_accessors.cc
@@ -225,7 +225,7 @@ void WriteAccessorsInSource(const protobuf::Descriptor* desc, Output& output) {
         output(
             R"cc(
               $1 $0::$2() const {
-                return ::hpb::UpbStrToStringView($3_$4(msg_));
+                return hpb::interop::upb::FromUpbStringView($3_$4(msg_));
               }
             )cc",
             class_name, CppConstType(field), resolved_field_name,

hpb_generator/gen_repeated_fields.cc
@@ -204,7 +204,7 @@ void WriteRepeatedStringAccessor(const protobuf::Descriptor* message,
           size_t len;
           auto* ptr = $3_mutable_$4(msg_, &len);
           assert(index < len);
-          return ::hpb::UpbStrToStringView(*(ptr + index));
+          return hpb::interop::upb::FromUpbStringView(*(ptr + index));
         }
       )cc",
       class_name, CppConstType(field), resolved_field_name,