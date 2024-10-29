protocolbuffers/protobuf

Implement GetExtension for numeric (int32 and int64)
At the moment, hpb's public api solely returns Ptr<const Extension>. We'd like to support all non-msg types like int32, int64, bool etc.
These should not return a Ptr<...> but the underlying primitive itself.

We start by adding support for int32 and int64.

PiperOrigin-RevId: 686615259

hpb_generator/tests/test_generated.cc
@@ -25,6 +25,7 @@
 #include "google/protobuf/compiler/hpb/tests/test_model.upb.proto.h"
 #include "google/protobuf/hpb/arena.h"
 #include "google/protobuf/hpb/backend/upb/interop.h"
+#include "google/protobuf/hpb/extension.h"
 #include "google/protobuf/hpb/hpb.h"
 #include "google/protobuf/hpb/ptr.h"
 #include "google/protobuf/hpb/repeated_field.h"
@@ -52,6 +53,7 @@ using ::hpb_unittest::protos::TestModel_Category_NEWS;
 using ::hpb_unittest::protos::TestModel_Category_VIDEO;
 using ::hpb_unittest::protos::theme;
 using ::hpb_unittest::protos::ThemeExtension;
+using ::hpb_unittest::someotherpackage::protos::int_ext;
 using ::testing::ElementsAre;
 
 TEST(CppGeneratedCode, Constructor) { TestModel test_model; }
@@ -874,6 +876,13 @@ TEST(CppGeneratedCode, GetExtension) {
   EXPECT_EQ(true, ::hpb::SetExtension(&model, theme, extension1).ok());
   EXPECT_EQ("Hello World",
             hpb::GetExtension(&model, theme).value()->ext_name());
+}
+
+TEST(CppGeneratedCode, GetExtensionInt32) {
+  TestModel model;
+  auto res = hpb::GetExtensionNumeric(&model, int_ext);
+  EXPECT_TRUE(res.ok());
+  EXPECT_EQ(res.value(), 0);
 }
 
 TEST(CppGeneratedCode, GetExtensionOnMutableChild) {

CodeLinaro-mirror/yocto-mirrors_github_protocolbuffers_protobuf

hpb_generator/tests/test_generated.cc
@@ -54,6 +54,7 @@ using ::hpb_unittest::protos::TestModel_Category_NEWS;
 using ::hpb_unittest::protos::TestModel_Category_VIDEO;
 using ::hpb_unittest::protos::theme;
 using ::hpb_unittest::protos::ThemeExtension;
+using ::hpb_unittest::someotherpackage::protos::int_ext;
 using ::testing::ElementsAre;
 
 TEST(CppGeneratedCode, Constructor) { TestModel test_model; }
@@ -876,6 +877,13 @@ TEST(CppGeneratedCode, GetExtension) {
   EXPECT_EQ(true, ::hpb::SetExtension(&model, theme, extension1).ok());
   EXPECT_EQ("Hello World",
             hpb::GetExtension(&model, theme).value()->ext_name());
+}
+
+TEST(CppGeneratedCode, GetExtensionInt32WithDefault) {
+  TestModel model;
+  auto res = hpb::GetExtension(&model, int_ext);
+  EXPECT_TRUE(res.ok());
+  EXPECT_EQ(res.value(), 644);
 }
 
 TEST(CppGeneratedCode, GetExtensionOnMutableChild) {