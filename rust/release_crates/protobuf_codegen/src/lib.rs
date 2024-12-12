c4468b355d94ac46bf1a47d55eabc4a8b55b9b02
Adam Cozzette <acozzette@google.com>
2024-12-11 16:59:04 -0800

    Rust: update protobuf_codegen crate to rely on separately installed protoc
    PiperOrigin-RevId: 705288486

--- rust/release_crates/protobuf_codegen/src/lib.rs
+++ rust/release_crates/protobuf_codegen/src/lib.rs
@@ -4,8 +4,6 @@ use std::path::{Path, PathBuf};
 pub struct CodeGen {
     inputs: Vec<PathBuf>,
     output_dir: PathBuf,
-    protoc_path: Option<PathBuf>,
-    protoc_gen_upb_minitable_path: Option<PathBuf>,
     includes: Vec<PathBuf>,
 }
 
@@ -16,8 +14,6 @@ impl CodeGen {
         Self {
             inputs: Vec::new(),
             output_dir: PathBuf::from(std::env::var("OUT_DIR").unwrap()).join("protobuf_generated"),
-            protoc_path: None,
-            protoc_gen_upb_minitable_path: None,
             includes: Vec::new(),
         }
     }
@@ -34,20 +30,6 @@ impl CodeGen {
 
     pub fn output_dir(&mut self, output_dir: impl AsRef<Path>) -> &mut Self {
         self.output_dir = output_dir.as_ref().to_owned();
-        self
-    }
-
-    pub fn protoc_path(&mut self, protoc_path: impl AsRef<Path>) -> &mut Self {
-        self.protoc_path = Some(protoc_path.as_ref().to_owned());
-        self
-    }
-
-    pub fn protoc_gen_upb_minitable_path(
-        &mut self,
-        protoc_gen_upb_minitable_path: impl AsRef<Path>,
-    ) -> &mut Self {
-        self.protoc_gen_upb_minitable_path =
-            Some(protoc_gen_upb_minitable_path.as_ref().to_owned());
         self
     }
 
@@ -83,6 +65,33 @@ impl CodeGen {
             .collect()
     }
 
+    fn protoc_version() -> String {
+        let output = std::process::Command::new("protoc").arg("--version").output().unwrap().stdout;
+
+        // The output of protoc --version looks something like "libprotoc XX.Y", with a
+        // possible suffix starting with a dash. We want to return just the
+        // "XX.Y" part.
+        let mut s =
+            String::from_utf8(output).unwrap().strip_prefix("libprotoc ").unwrap().to_string();
+        let first_dash = s.find('-');
+        if let Some(i) = first_dash {
+            s.truncate(i);
+        }
+        s
+    }
+
+    fn expected_protoc_version() -> String {
+        let mut s = VERSION.to_string();
+        let first_dash = s.find('-');
+        if let Some(i) = first_dash {
+            s.truncate(i);
+        }
+        let mut v: Vec<&str> = s.split('.').collect();
+        assert_eq!(v.len(), 3);
+        v.remove(0);
+        v.join(".")
+    }
+
     pub fn generate_and_compile(&self) -> Result<(), String> {
         let upb_version = std::env::var("DEP_UPB_VERSION").expect("DEP_UPB_VERSION should have been set, make sure that the Protobuf crate is a dependency");
         if VERSION != upb_version {
@@ -92,12 +101,16 @@ impl CodeGen {
             );
         }
 
-        let protoc_path = if let Some(path) = &self.protoc_path {
-            path.clone()
-        } else {
-            protoc_path().expect("To be a supported platform")
-        };
-        let mut cmd = std::process::Command::new(protoc_path);
+        let protoc_version = Self::protoc_version();
+        let expected_protoc_version = Self::expected_protoc_version();
+        if protoc_version != expected_protoc_version {
+            panic!(
+                "Expected protoc version {} but found {}",
+                expected_protoc_version, protoc_version
+            );
+        }
+
+        let mut cmd = std::process::Command::new("protoc");
         for input in &self.inputs {
             cmd.arg(input);
         }
@@ -105,12 +118,6 @@ impl CodeGen {
             // Attempt to make the directory if it doesn't exist
             let _ = std::fs::create_dir(&self.output_dir);
         }
-        let protoc_gen_upb_minitable_path = if let Some(path) = &self.protoc_gen_upb_minitable_path
-        {
-            path.clone()
-        } else {
-            protoc_gen_upb_minitable_path().expect("To be a supported platform")
-        };
 
         for include in &self.includes {
             println!("cargo:rerun-if-changed={}", include.display());
@@ -118,10 +125,6 @@ impl CodeGen {
 
         cmd.arg(format!("--rust_out={}", self.output_dir.display()))
             .arg("--rust_opt=experimental-codegen=enabled,kernel=upb")
-            .arg(format!(
-                "--plugin=protoc-gen-upb_minitable={}",
-                protoc_gen_upb_minitable_path.display()
-            ))
             .arg(format!("--upb_minitable_out={}", self.output_dir.display()));
         for include in &self.includes {
             cmd.arg(format!("--proto_path={}", include.display()));
@@ -160,34 +163,4 @@ impl CodeGen {
         cc_build.compile(&format!("{}_upb_gen_code", std::env::var("CARGO_PKG_NAME").unwrap()));
         Ok(())
     }
-}
-
-fn get_path_for_arch() -> Option<PathBuf> {
-    let mut path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-    path.push("bin");
-    match (std::env::consts::OS, std::env::consts::ARCH) {
-        ("macos", "x86_64") => path.push("osx-x86_64"),
-        ("macos", "aarch64") => path.push("osx-aarch_64"),
-        ("linux", "aarch64") => path.push("linux-aarch_64"),
-        ("linux", "powerpc64") => path.push("linux-ppcle_64"),
-        ("linux", "s390x") => path.push("linux-s390_64"),
-        ("linux", "x86") => path.push("linux-x86_32"),
-        ("linux", "x86_64") => path.push("linux-x86_64"),
-        ("windows", "x86") => path.push("win32"),
-        ("windows", "x86_64") => path.push("win64"),
-        _ => return None,
-    };
-    Some(path)
-}
-
-pub fn protoc_path() -> Option<PathBuf> {
-    let mut path = get_path_for_arch()?;
-    path.push("protoc");
-    Some(path)
-}
-
-pub fn protoc_gen_upb_minitable_path() -> Option<PathBuf> {
-    let mut path = get_path_for_arch()?;
-    path.push("protoc-gen-upb_minitable");
-    Some(path)
 }
