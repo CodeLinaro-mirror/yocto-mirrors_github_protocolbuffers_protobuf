0884c1fc12c19fe36520440fc628ab06c92f428a
Clayton Knittel <cknittel@google.com>
2025-10-21 16:13:40 -0700

    Account for 8-byte alignment in arena allocation size checks.
    Arenas allocate aligned to 8 bytes on both 32-bit and 64-bit platforms, but message/field types may not be 8-byte aligned on 32-bit. If their size happens to not be a multiple of 8, these tests will fail.
    
    PiperOrigin-RevId: 822316441

--- src/google/protobuf/arena_unittest.cc
+++ src/google/protobuf/arena_unittest.cc
@@ -68,6 +68,13 @@ using ::testing::HasSubstr;
 namespace google {
 namespace protobuf {
 
+namespace {
+
+// Align n to next multiple of 8
+constexpr uint64_t Align8(uint64_t n) { return (n + 7) & -8; }
+
+}  // namespace
+
 
 class Notifier {
  public:
@@ -444,8 +451,8 @@ TEST(ArenaTest, MoveCtorOnArena) {
   TestUtil::ExpectAllFieldsSet(moved->payload());
 
   // The only extra allocation with moves is sizeof(NestedTestAllTypes).
-  EXPECT_EQ(usage_by_move, sizeof(NestedTestAllTypes));
-  EXPECT_LT(usage_by_move + sizeof(TestAllTypes), usage_original);
+  EXPECT_EQ(usage_by_move, Align8(sizeof(NestedTestAllTypes)));
+  EXPECT_LT(usage_by_move + Align8(sizeof(TestAllTypes)), usage_original);
 
   // Status after move is unspecified and must not be assumed. It's merely
   // checking current implementation specifics for protobuf internal.
@@ -495,11 +502,11 @@ TEST(ArenaTest, RepeatedPtrFieldMoveCtorOnArena) {
   // The only extra allocation with moves is sizeof(RepeatedPtrField).
 #ifdef PROTOBUF_INTERNAL_REMOVE_ARENA_PTRS_REPEATED_PTR_FIELD
   EXPECT_EQ(usage_by_move,
-            sizeof(internal::RepeatedPtrFieldWithArena<TestAllTypes>));
+            Align8(sizeof(internal::RepeatedPtrFieldWithArena<TestAllTypes>)));
 #else
-  EXPECT_EQ(usage_by_move, sizeof(internal::RepeatedPtrFieldBase));
+  EXPECT_EQ(usage_by_move, Align8(sizeof(internal::RepeatedPtrFieldBase)));
 #endif
-  EXPECT_LT(usage_by_move + sizeof(TestAllTypes), usage_original);
+  EXPECT_LT(usage_by_move + Align8(sizeof(TestAllTypes)), usage_original);
 
   // Status after move is unspecified and must not be assumed. It's merely
   // checking current implementation specifics for protobuf internal.
@@ -1694,9 +1701,6 @@ TEST(ArenaTest, MessageLiteOnArena) {
   arena.Reset();
 }
 #endif  // PROTOBUF_RTTI
-
-// Align n to next multiple of 8
-uint64_t Align8(uint64_t n) { return (n + 7) & -8; }
 
 TEST(ArenaTest, SpaceAllocated_and_Used) {
   Arena arena_1;
