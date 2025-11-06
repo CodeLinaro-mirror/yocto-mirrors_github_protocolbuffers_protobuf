bc02523f925bb38504468c5f29460e6347a0c1fb
Mike Kruskal <mkruskal@google.com>
2025-11-06 10:48:23 -0800

    Expose NestedInFileClass naming helpers for Java immutable.
    This matches our GeneratorNames APIs, and it's become clear that this information is necessary for certain classes of code-generators to be able to predict where our generated code will end up.
    
    PiperOrigin-RevId: 829020588

---
+++ upb/mini_table/internal/generated_registry.c
@@ -0,0 +1,7 @@
+#include "upb/mini_table/internal/generated_registry.h"
+
+// Must be last.
+#include "upb/port/def.inc"
+
+const upb_GeneratedExtensionListEntry* UPB_PRIVATE(
+    upb_generated_extension_list) = NULL;
