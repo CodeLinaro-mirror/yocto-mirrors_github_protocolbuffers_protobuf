CodeLinaro-mirror/yocto-mirrors_protobuf

Remove --record_requests and RecordingTestRunner from conformance_test_runner.
The request golden that compared a recording of every request before and after each migration step was deleted in the previous CL, and this flag and the runner that implemented it existed only for that golden (and for producing the old-name-to-new-name mapping the previous CL rewrote the failure lists from). Nothing else reads the recordings: the flag is dropped from ConformanceRunnerOptions, the parser, the usage text and the runner's main(), RecordingTestRunner and its test are deleted, conformance_test_runner_test.cc drops the flag from its defaults, every-flag, missing-value and usage checks, and merged_runner_test.sh loses the scenario that exercised the flag (the later scenarios are renumbered).

PiperOrigin-RevId: 984706287

conformance/binary_unstable_edition_test.cc
@@ -1,0 +1,87 @@
+// Protocol Buffers - Google's data interchange format
+// Copyright 2025 Google LLC.  All rights reserved.
+//
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file or at
+// https://developers.google.com/open-source/licenses/bsd
+
+// Binary conformance tests for TestAllTypesEditionUnstable, the test message
+// declared under the unstable edition.  This holds both the binary-output and
+// the JSON-output leg of the legacy suite-level
+// BinaryAndJsonConformanceSuite::RunUnstableTests(); the requests sent to the
+// testee are identical to the legacy ones.
+//
+// Like the legacy suite, these run whenever --maximum_edition is 2023 or
+// newer (see ConformanceTest::IsSupported()), not only when it names the
+// unstable edition itself.
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+#include "conformance/binary_wireformat.h"
+#include "conformance/matchers.h"
+#include "conformance/test_environment.h"
+#include "conformance/test_protos/test_messages_edition_unstable.pb.h"
+#include "google/protobuf/descriptor.h"
+
+namespace google {
+namespace protobuf {
+namespace conformance {
+namespace {
+
+using ::protobuf_test_messages::edition_unstable::TestAllTypesEditionUnstable;
+
+// Reporting TestAllTypesEditionUnstable through MessageUnderTest() makes the
+// base SetUp() skip these tests when --maximum_edition doesn't cover it.
+class UnstableEditionTest : public ConformanceTest {
+ protected:
+  const Descriptor* MessageUnderTest() const override { return message(); }
+
+  static const Descriptor* message() {
+    return TestAllTypesEditionUnstable::descriptor();
+  }
+};
+
+// Field 13 is optional_bytes.
+TEST_F(UnstableEditionTest, ValidBytes) {
+  EXPECT_THAT(
+      RequiredTest()
+          .ParseBinary(message(), LengthPrefixedField(13, "foo"))
+          .SerializeBinary(),
+      Yields(ParsedPayload(EqualsTextProto(R"pb(optional_bytes: "foo")pb"))));
+}
+
+TEST_F(UnstableEditionTest, ValidBytesJson) {
+  EXPECT_THAT(
+      RequiredTest()
+          .ParseBinary(message(), LengthPrefixedField(13, "foo"))
+          .SerializeJson(),
+      Yields(ParsedPayload(EqualsTextProto(R"pb(optional_bytes: "foo")pb"))));
+}
+
+// Field 15 is map_string_bytes; its entries have key = 1 and value = 2.
+TEST_F(UnstableEditionTest, ValidMapBytes) {
+  EXPECT_THAT(RequiredTest()
+                  .ParseBinary(message(),
+                               LengthPrefixedField(
+                                   15, Wire(LengthPrefixedField(1, "foo"),
+                                            LengthPrefixedField(2, "barbaz"))))
+                  .SerializeBinary(),
+              Yields(ParsedPayload(EqualsTextProto(
+                  R"pb(map_string_bytes { key: "foo" value: "barbaz" })pb"))));
+}
+
+TEST_F(UnstableEditionTest, ValidMapBytesJson) {
+  EXPECT_THAT(RequiredTest()
+                  .ParseBinary(message(),
+                               LengthPrefixedField(
+                                   15, Wire(LengthPrefixedField(1, "foo"),
+                                            LengthPrefixedField(2, "barbaz"))))
+                  .SerializeJson(),
+              Yields(ParsedPayload(EqualsTextProto(
+                  R"pb(map_string_bytes { key: "foo" value: "barbaz" })pb"))));
+}
+
+}  // namespace
+}  // namespace conformance
+}  // namespace protobuf
+}  // namespace google

conformance/json_hello_world_test.cc
@@ -1,0 +1,108 @@
+// Protocol Buffers - Google's data interchange format
+// Copyright 2025 Google LLC.  All rights reserved.
+//
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file or at
+// https://developers.google.com/open-source/licenses/bsd
+
+// The smoke tests of the JSON conformance suite: a single string field
+// round-trips through the testee, unknown members of every JSON kind are
+// ignored when asked to, and a top-level `null` is rejected.  This replaces
+// the tests the legacy BinaryAndJsonConformanceSuiteImpl<M>::RunJsonTests()
+// ran directly (rather than through one of its RunJsonTestsFor*() groups),
+// for every test message type; the requests sent to the testee are identical to
+// the legacy ones.
+//
+// Like the legacy RunValidJsonTest(), each valid input is sent twice: once to
+// be serialized as binary ("<name>.ProtobufOutput") and once as JSON
+// ("<name>.JsonOutput").  The legacy RunValidJsonIgnoreUnknownTest() only had
+// the binary leg, and ExpectParseFailureForJson() asked for JSON output under
+// a name without an output suffix.
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+#include "absl/strings/string_view.h"
+#include "conformance/binary_test_util.h"
+#include "conformance/matchers.h"
+#include "conformance/message_type_fixtures.h"
+#include "conformance/test_environment.h"
+
+namespace google {
+namespace protobuf {
+namespace conformance {
+namespace {
+
+using ::testing::ValuesIn;
+
+using JsonHelloWorldTest = MessageTypeConformanceTest;
+
+TEST_P(JsonHelloWorldTest, HelloWorld) {
+  constexpr absl::string_view kInput = R"({"optionalString":"Hello, World!"})";
+  constexpr absl::string_view kExpected = "optional_string: 'Hello, World!'";
+  EXPECT_THAT(RequiredTest().ParseJson(message(), kInput).SerializeBinary(),
+              Yields(ParsedPayload(EqualsTextProto(kExpected))));
+  EXPECT_THAT(RequiredTest().ParseJson(message(), kInput).SerializeJson(),
+              Yields(ParsedPayload(EqualsTextProto(kExpected))));
+}
+
+// Unknown members are ignored whatever kind of JSON value they hold.
+TEST_P(JsonHelloWorldTest, IgnoreUnknownJsonNumber) {
+  EXPECT_THAT(RequiredTest()
+                  .ParseJson(message(), R"({"unknown": 1})",
+                             {.ignore_unknown_fields = true})
+                  .SerializeBinary(),
+              Yields(ParsedPayload(EqualsTextProto(""))));
+}
+
+TEST_P(JsonHelloWorldTest, IgnoreUnknownJsonString) {
+  EXPECT_THAT(RequiredTest()
+                  .ParseJson(message(), R"({"unknown": "a"})",
+                             {.ignore_unknown_fields = true})
+                  .SerializeBinary(),
+              Yields(ParsedPayload(EqualsTextProto(""))));
+}
+
+TEST_P(JsonHelloWorldTest, IgnoreUnknownJsonTrue) {
+  EXPECT_THAT(RequiredTest()
+                  .ParseJson(message(), R"({"unknown": true})",
+                             {.ignore_unknown_fields = true})
+                  .SerializeBinary(),
+              Yields(ParsedPayload(EqualsTextProto(""))));
+}
+
+TEST_P(JsonHelloWorldTest, IgnoreUnknownJsonFalse) {
+  EXPECT_THAT(RequiredTest()
+                  .ParseJson(message(), R"({"unknown": false})",
+                             {.ignore_unknown_fields = true})
+                  .SerializeBinary(),
+              Yields(ParsedPayload(EqualsTextProto(""))));
+}
+
+TEST_P(JsonHelloWorldTest, IgnoreUnknownJsonNull) {
+  EXPECT_THAT(RequiredTest()
+                  .ParseJson(message(), R"({"unknown": null})",
+                             {.ignore_unknown_fields = true})
+                  .SerializeBinary(),
+              Yields(ParsedPayload(EqualsTextProto(""))));
+}
+
+TEST_P(JsonHelloWorldTest, IgnoreUnknownJsonObject) {
+  EXPECT_THAT(RequiredTest()
+                  .ParseJson(message(), R"({"unknown": {"a": 1}})",
+                             {.ignore_unknown_fields = true})
+                  .SerializeBinary(),
+              Yields(ParsedPayload(EqualsTextProto(""))));
+}
+
+TEST_P(JsonHelloWorldTest, RejectTopLevelNull) {
+  EXPECT_THAT(RequiredTest().ParseJson(message(), "null").ParseOnly(),
+              Yields(IsParseError()));
+}
+
+INSTANTIATE_TEST_SUITE_P(All, JsonHelloWorldTest,
+                         ValuesIn(AllTestMessageTypes()), MessageTypeParamName);
+
+}  // namespace
+}  // namespace conformance
+}  // namespace protobuf
+}  // namespace google

conformance/text_enum_test.cc
@@ -1,0 +1,89 @@
+// Protocol Buffers - Google's data interchange format
+// Copyright 2025 Google LLC.  All rights reserved.
+//
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file or at
+// https://developers.google.com/open-source/licenses/bsd
+
+// Text-format conformance tests for enum fields given by number.  An open
+// enum (proto3-style message types) keeps any number, a closed enum
+// (proto2-style message types) rejects numbers that aren't one of its values.
+// This replaces the legacy TextFormatConformanceTestSuiteImpl<M>::
+// RunOpenEnumTests() and RunClosedEnumTests(); the requests sent to the testee
+// are identical to the legacy ones.
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+#include "absl/strings/string_view.h"
+#include "conformance/binary_test_util.h"
+#include "conformance/matchers.h"
+#include "conformance/message_type_fixtures.h"
+#include "conformance/test_environment.h"
+
+namespace google {
+namespace protobuf {
+namespace conformance {
+namespace {
+
+using ::testing::ValuesIn;
+
+// The legacy inputs were multi-line raw string literals; their surrounding
+// whitespace is part of the request bytes and is kept verbatim.
+constexpr absl::string_view kKnownNumberInput =
+    "\n        optional_nested_enum: 1\n        ";
+constexpr absl::string_view kUnknownNumberInput =
+    "\n        optional_nested_enum: 42\n        ";
+
+// Note: the legacy suite named these tests "ClosedEnum..." for the open-enum
+// message types too, so the names stay.
+
+using TextOpenEnumTest = MessageTypeConformanceTest;
+
+TEST_P(TextOpenEnumTest, ByNumber) {
+  EXPECT_THAT(
+      RequiredTest().ParseText(message(), kKnownNumberInput).SerializeBinary(),
+      Yields(ParsedPayload(EqualsTextProto(kKnownNumberInput))));
+  EXPECT_THAT(
+      RequiredTest().ParseText(message(), kKnownNumberInput).SerializeText(),
+      Yields(ParsedPayload(EqualsTextProto(kKnownNumberInput))));
+}
+
+TEST_P(TextOpenEnumTest, WithUnknownNumber) {
+  EXPECT_THAT(RequiredTest()
+                  .ParseText(message(), kUnknownNumberInput)
+                  .SerializeBinary(),
+              Yields(ParsedPayload(EqualsTextProto(kUnknownNumberInput))));
+  EXPECT_THAT(
+      RequiredTest().ParseText(message(), kUnknownNumberInput).SerializeText(),
+      Yields(ParsedPayload(EqualsTextProto(kUnknownNumberInput))));
+}
+
+INSTANTIATE_TEST_SUITE_P(All, TextOpenEnumTest,
+                         ValuesIn(Proto3TestMessageTypes()),
+                         MessageTypeParamName);
+
+using TextClosedEnumTest = MessageTypeConformanceTest;
+
+TEST_P(TextClosedEnumTest, ByNumber) {
+  EXPECT_THAT(
+      RequiredTest().ParseText(message(), kKnownNumberInput).SerializeBinary(),
+      Yields(ParsedPayload(EqualsTextProto(kKnownNumberInput))));
+  EXPECT_THAT(
+      RequiredTest().ParseText(message(), kKnownNumberInput).SerializeText(),
+      Yields(ParsedPayload(EqualsTextProto(kKnownNumberInput))));
+}
+
+TEST_P(TextClosedEnumTest, WithUnknownNumber) {
+  EXPECT_THAT(
+      RequiredTest().ParseText(message(), kUnknownNumberInput).ParseOnly(),
+      Yields(IsParseError()));
+}
+
+INSTANTIATE_TEST_SUITE_P(All, TextClosedEnumTest,
+                         ValuesIn(Proto2TestMessageTypes()),
+                         MessageTypeParamName);
+
+}  // namespace
+}  // namespace conformance
+}  // namespace protobuf
+}  // namespace google

CodeLinaro-mirror/yocto-mirrors_github_protocolbuffers_protobuf

conformance/binary_unstable_edition_test.cc
@@ -1,0 +1,87 @@
+// Protocol Buffers - Google's data interchange format
+// Copyright 2025 Google LLC.  All rights reserved.
+//
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file or at
+// https://developers.google.com/open-source/licenses/bsd
+
+// Binary conformance tests for TestAllTypesEditionUnstable, the test message
+// declared under the unstable edition.  This holds both the binary-output and
+// the JSON-output leg of the legacy suite-level
+// BinaryAndJsonConformanceSuite::RunUnstableTests(); the requests sent to the
+// testee are identical to the legacy ones.
+//
+// Like the legacy suite, these run whenever --maximum_edition is 2023 or
+// newer (see ConformanceTest::IsSupported()), not only when it names the
+// unstable edition itself.
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+#include "conformance/binary_wireformat.h"
+#include "conformance/matchers.h"
+#include "conformance/test_environment.h"
+#include "conformance/test_protos/test_messages_edition_unstable.pb.h"
+#include "google/protobuf/descriptor.h"
+
+namespace google {
+namespace protobuf {
+namespace conformance {
+namespace {
+
+using ::protobuf_test_messages::edition_unstable::TestAllTypesEditionUnstable;
+
+// Reporting TestAllTypesEditionUnstable through MessageUnderTest() makes the
+// base SetUp() skip these tests when --maximum_edition doesn't cover it.
+class UnstableEditionTest : public ConformanceTest {
+ protected:
+  const Descriptor* MessageUnderTest() const override { return message(); }
+
+  static const Descriptor* message() {
+    return TestAllTypesEditionUnstable::descriptor();
+  }
+};
+
+// Field 13 is optional_bytes.
+TEST_F(UnstableEditionTest, ValidBytes) {
+  EXPECT_THAT(
+      Testee()
+          .ParseBinary(message(), LengthPrefixedField(13, "foo"))
+          .SerializeBinary(),
+      Yields(ParsedPayload(EqualsTextProto(R"pb(optional_bytes: "foo")pb"))));
+}
+
+TEST_F(UnstableEditionTest, ValidBytesJson) {
+  EXPECT_THAT(
+      Testee()
+          .ParseBinary(message(), LengthPrefixedField(13, "foo"))
+          .SerializeJson(),
+      Yields(ParsedPayload(EqualsTextProto(R"pb(optional_bytes: "foo")pb"))));
+}
+
+// Field 15 is map_string_bytes; its entries have key = 1 and value = 2.
+TEST_F(UnstableEditionTest, ValidMapBytes) {
+  EXPECT_THAT(Testee()
+                  .ParseBinary(message(),
+                               LengthPrefixedField(
+                                   15, Wire(LengthPrefixedField(1, "foo"),
+                                            LengthPrefixedField(2, "barbaz"))))
+                  .SerializeBinary(),
+              Yields(ParsedPayload(EqualsTextProto(
+                  R"pb(map_string_bytes { key: "foo" value: "barbaz" })pb"))));
+}
+
+TEST_F(UnstableEditionTest, ValidMapBytesJson) {
+  EXPECT_THAT(Testee()
+                  .ParseBinary(message(),
+                               LengthPrefixedField(
+                                   15, Wire(LengthPrefixedField(1, "foo"),
+                                            LengthPrefixedField(2, "barbaz"))))
+                  .SerializeJson(),
+              Yields(ParsedPayload(EqualsTextProto(
+                  R"pb(map_string_bytes { key: "foo" value: "barbaz" })pb"))));
+}
+
+}  // namespace
+}  // namespace conformance
+}  // namespace protobuf
+}  // namespace google

conformance/json_hello_world_test.cc
@@ -1,0 +1,108 @@
+// Protocol Buffers - Google's data interchange format
+// Copyright 2025 Google LLC.  All rights reserved.
+//
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file or at
+// https://developers.google.com/open-source/licenses/bsd
+
+// The smoke tests of the JSON conformance suite: a single string field
+// round-trips through the testee, unknown members of every JSON kind are
+// ignored when asked to, and a top-level `null` is rejected.  This replaces
+// the tests the legacy BinaryAndJsonConformanceSuiteImpl<M>::RunJsonTests()
+// ran directly (rather than through one of its RunJsonTestsFor*() groups),
+// for every test message type; the requests sent to the testee are identical to
+// the legacy ones.
+//
+// Like the legacy RunValidJsonTest(), each valid input is sent twice: once to
+// be serialized as binary ("<name>.ProtobufOutput") and once as JSON
+// ("<name>.JsonOutput").  The legacy RunValidJsonIgnoreUnknownTest() only had
+// the binary leg, and ExpectParseFailureForJson() asked for JSON output under
+// a name without an output suffix.
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+#include "absl/strings/string_view.h"
+#include "conformance/binary_test_util.h"
+#include "conformance/matchers.h"
+#include "conformance/message_type_fixtures.h"
+#include "conformance/test_environment.h"
+
+namespace google {
+namespace protobuf {
+namespace conformance {
+namespace {
+
+using ::testing::ValuesIn;
+
+using JsonHelloWorldTest = MessageTypeConformanceTest;
+
+TEST_P(JsonHelloWorldTest, HelloWorld) {
+  constexpr absl::string_view kInput = R"({"optionalString":"Hello, World!"})";
+  constexpr absl::string_view kExpected = "optional_string: 'Hello, World!'";
+  EXPECT_THAT(Testee().ParseJson(message(), kInput).SerializeBinary(),
+              Yields(ParsedPayload(EqualsTextProto(kExpected))));
+  EXPECT_THAT(Testee().ParseJson(message(), kInput).SerializeJson(),
+              Yields(ParsedPayload(EqualsTextProto(kExpected))));
+}
+
+// Unknown members are ignored whatever kind of JSON value they hold.
+TEST_P(JsonHelloWorldTest, IgnoreUnknownJsonNumber) {
+  EXPECT_THAT(Testee()
+                  .ParseJson(message(), R"({"unknown": 1})",
+                             {/*ignore_unknown_fields=*/true})
+                  .SerializeBinary(),
+              Yields(ParsedPayload(EqualsTextProto(""))));
+}
+
+TEST_P(JsonHelloWorldTest, IgnoreUnknownJsonString) {
+  EXPECT_THAT(Testee()
+                  .ParseJson(message(), R"({"unknown": "a"})",
+                             {/*ignore_unknown_fields=*/true})
+                  .SerializeBinary(),
+              Yields(ParsedPayload(EqualsTextProto(""))));
+}
+
+TEST_P(JsonHelloWorldTest, IgnoreUnknownJsonTrue) {
+  EXPECT_THAT(Testee()
+                  .ParseJson(message(), R"({"unknown": true})",
+                             {/*ignore_unknown_fields=*/true})
+                  .SerializeBinary(),
+              Yields(ParsedPayload(EqualsTextProto(""))));
+}
+
+TEST_P(JsonHelloWorldTest, IgnoreUnknownJsonFalse) {
+  EXPECT_THAT(Testee()
+                  .ParseJson(message(), R"({"unknown": false})",
+                             {/*ignore_unknown_fields=*/true})
+                  .SerializeBinary(),
+              Yields(ParsedPayload(EqualsTextProto(""))));
+}
+
+TEST_P(JsonHelloWorldTest, IgnoreUnknownJsonNull) {
+  EXPECT_THAT(Testee()
+                  .ParseJson(message(), R"({"unknown": null})",
+                             {/*ignore_unknown_fields=*/true})
+                  .SerializeBinary(),
+              Yields(ParsedPayload(EqualsTextProto(""))));
+}
+
+TEST_P(JsonHelloWorldTest, IgnoreUnknownJsonObject) {
+  EXPECT_THAT(Testee()
+                  .ParseJson(message(), R"({"unknown": {"a": 1}})",
+                             {/*ignore_unknown_fields=*/true})
+                  .SerializeBinary(),
+              Yields(ParsedPayload(EqualsTextProto(""))));
+}
+
+TEST_P(JsonHelloWorldTest, RejectTopLevelNull) {
+  EXPECT_THAT(Testee().ParseJson(message(), "null").ParseOnly(),
+              Yields(IsParseError()));
+}
+
+INSTANTIATE_TEST_SUITE_P(All, JsonHelloWorldTest,
+                         ValuesIn(AllTestMessageTypes()), MessageTypeParamName);
+
+}  // namespace
+}  // namespace conformance
+}  // namespace protobuf
+}  // namespace google

conformance/text_enum_test.cc
@@ -1,0 +1,85 @@
+// Protocol Buffers - Google's data interchange format
+// Copyright 2025 Google LLC.  All rights reserved.
+//
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file or at
+// https://developers.google.com/open-source/licenses/bsd
+
+// Text-format conformance tests for enum fields given by number.  An open
+// enum (proto3-style message types) keeps any number, a closed enum
+// (proto2-style message types) rejects numbers that aren't one of its values.
+// This replaces the legacy TextFormatConformanceTestSuiteImpl<M>::
+// RunOpenEnumTests() and RunClosedEnumTests(); the requests sent to the testee
+// are identical to the legacy ones.
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+#include "absl/strings/string_view.h"
+#include "conformance/binary_test_util.h"
+#include "conformance/matchers.h"
+#include "conformance/message_type_fixtures.h"
+#include "conformance/test_environment.h"
+
+namespace google {
+namespace protobuf {
+namespace conformance {
+namespace {
+
+using ::testing::ValuesIn;
+
+// The legacy inputs were multi-line raw string literals; their surrounding
+// whitespace is part of the request bytes and is kept verbatim.
+constexpr absl::string_view kKnownNumberInput =
+    "\n        optional_nested_enum: 1\n        ";
+constexpr absl::string_view kUnknownNumberInput =
+    "\n        optional_nested_enum: 42\n        ";
+
+// Note: the legacy suite named these tests "ClosedEnum..." for the open-enum
+// message types too, so the names stay.
+
+using TextOpenEnumTest = MessageTypeConformanceTest;
+
+TEST_P(TextOpenEnumTest, ByNumber) {
+  EXPECT_THAT(
+      Testee().ParseText(message(), kKnownNumberInput).SerializeBinary(),
+      Yields(ParsedPayload(EqualsTextProto(kKnownNumberInput))));
+  EXPECT_THAT(Testee().ParseText(message(), kKnownNumberInput).SerializeText(),
+              Yields(ParsedPayload(EqualsTextProto(kKnownNumberInput))));
+}
+
+TEST_P(TextOpenEnumTest, WithUnknownNumber) {
+  EXPECT_THAT(
+      Testee().ParseText(message(), kUnknownNumberInput).SerializeBinary(),
+      Yields(ParsedPayload(EqualsTextProto(kUnknownNumberInput))));
+  EXPECT_THAT(
+      Testee().ParseText(message(), kUnknownNumberInput).SerializeText(),
+      Yields(ParsedPayload(EqualsTextProto(kUnknownNumberInput))));
+}
+
+INSTANTIATE_TEST_SUITE_P(All, TextOpenEnumTest,
+                         ValuesIn(Proto3TestMessageTypes()),
+                         MessageTypeParamName);
+
+using TextClosedEnumTest = MessageTypeConformanceTest;
+
+TEST_P(TextClosedEnumTest, ByNumber) {
+  EXPECT_THAT(
+      Testee().ParseText(message(), kKnownNumberInput).SerializeBinary(),
+      Yields(ParsedPayload(EqualsTextProto(kKnownNumberInput))));
+  EXPECT_THAT(Testee().ParseText(message(), kKnownNumberInput).SerializeText(),
+              Yields(ParsedPayload(EqualsTextProto(kKnownNumberInput))));
+}
+
+TEST_P(TextClosedEnumTest, WithUnknownNumber) {
+  EXPECT_THAT(Testee().ParseText(message(), kUnknownNumberInput).ParseOnly(),
+              Yields(IsParseError()));
+}
+
+INSTANTIATE_TEST_SUITE_P(All, TextClosedEnumTest,
+                         ValuesIn(Proto2TestMessageTypes()),
+                         MessageTypeParamName);
+
+}  // namespace
+}  // namespace conformance
+}  // namespace protobuf
+}  // namespace google