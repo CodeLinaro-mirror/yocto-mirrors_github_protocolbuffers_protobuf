CodeLinaro-mirror/yocto-mirrors_protobuf

Move the Python conformance tests to the conformance_test() macro.
The six conformance_python{,_cpp,_upb}{,_performance}_test sh_tests, which ran conformance_test_runner through conformance_test_wrapper.sh, become conformance_test(name = "python"), :python_cpp and :python_upb, i.e. python_{binary,json,performance,text}_test and friends, grouped under one test_suite per testee.  The performance sh_tests need no target of their own: the macro's <name>_performance_test replaces them. Their tags are not carried over: the sh_tests' noasan/nomsan/notsan/noubsan (and python's nodiorite) worked around a runner-side stack overflow that is fixed (b/563731788), and the tests pass under the sanitizers; their noarm had no reason recorded, and the three python performance tests pass on ARM (--cpu=arm) in a few seconds each. --maximum_edition 2023 is kept.

The legacy --failure_list / --text_format_failure_list pairs are split into one list per suite under failure_lists/ (python_{binary,json,text}.txt, python_cpp_{binary,json,text}.txt, python_upb_{binary,json,text}.txt, python{,_cpp,_upb}_performance.txt), following the same rule as the cpp lists: text-format tests to the text list, JSON-input and the input-less serialize tests to the json list, the rest to the binary list.  The three testees shared text_format_failure_list_python.txt before; each now gets its own copy of the text split.  python_upb has no binary failures, so its python_upb_binary.txt only carries the commented-out TODO(b/477413000) block, next to the other testees' binary lists that hold the same block.  The performance headers that said "C++ TextFormat" now name the Python and Python/upb implementations.

failure_list_python.txt, failure_list_python_cpp.txt, failure_list_python_upb.txt and text_format_failure_list_python.txt stay for now: the open-source Bazel conformance targets still read them (so does Makefile.am, until it is deleted later in this chain).  The six performance lists had no other reader and ar

conformance/text_map_test.cc
@@ -1,0 +1,141 @@
+// Protocol Buffers - Google's data interchange format
+// Copyright 2025 Google LLC.  All rights reserved.
+//
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file or at
+// https://developers.google.com/open-source/licenses/bsd
+
+// Text-format conformance tests for map fields: entries given in key order
+// round-trip, and a duplicated key keeps the last value.  This replaces the
+// legacy map block of TextFormatConformanceTestSuiteImpl<M>::RunAllTests(),
+// which ran for the proto3-style message types only; the test names and the
+// requests sent to the testee are identical to the legacy ones.
+//
+// The legacy tests passed a prototype pre-filled with the map entries in
+// reverse order to RunValidTextFormatTestWithMessage(); that had no effect
+// (the legacy ConformanceRequestSetting only took the prototype's descriptor
+// and compared against prototype.New()), so nothing replaces it here.
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+#include "absl/strings/string_view.h"
+#include "conformance/binary_test_util.h"
+#include "conformance/matchers.h"
+#include "conformance/message_type_fixtures.h"
+#include "conformance/test_environment.h"
+
+namespace google {
+namespace protobuf {
+namespace conformance {
+namespace {
+
+using ::testing::ValuesIn;
+
+// The inputs below are the legacy raw string literals verbatim: their
+// surrounding whitespace and line breaks are part of the request bytes.
+
+using TextMapTest = MessageTypeConformanceTest;
+
+TEST_P(TextMapTest, AlphabeticallySortedMapStringKeys) {
+  constexpr absl::string_view kInput = R"(
+        map_string_string {
+          key: "a"
+          value: "value"
+        }
+        map_string_string {
+          key: "b"
+          value: "value"
+        }
+        map_string_string {
+          key: "c"
+          value: "value"
+        }
+        )";
+  EXPECT_THAT(RequiredTest("AlphabeticallySortedMapStringKeys")
+                  .ParseText(message(), kInput)
+                  .SerializeBinary(),
+              Yields(ParsedPayload(EqualsTextProto(kInput))));
+  EXPECT_THAT(RequiredTest("AlphabeticallySortedMapStringKeys")
+                  .ParseText(message(), kInput)
+                  .SerializeText(),
+              Yields(ParsedPayload(EqualsTextProto(kInput))));
+}
+
+TEST_P(TextMapTest, AlphabeticallySortedMapIntKeys) {
+  constexpr absl::string_view kInput = R"(
+        map_int32_int32 {
+          key: 1
+          value: 0
+        }
+        map_int32_int32 {
+          key: 2
+          value: 0
+        }
+        map_int32_int32 {
+          key: 3
+          value: 0
+        }
+        )";
+  EXPECT_THAT(RequiredTest("AlphabeticallySortedMapIntKeys")
+                  .ParseText(message(), kInput)
+                  .SerializeBinary(),
+              Yields(ParsedPayload(EqualsTextProto(kInput))));
+  EXPECT_THAT(RequiredTest("AlphabeticallySortedMapIntKeys")
+                  .ParseText(message(), kInput)
+                  .SerializeText(),
+              Yields(ParsedPayload(EqualsTextProto(kInput))));
+}
+
+TEST_P(TextMapTest, AlphabeticallySortedMapBoolKeys) {
+  constexpr absl::string_view kInput = R"(
+        map_bool_bool {
+          key: false
+          value: false
+        }
+        map_bool_bool {
+          key: true
+          value: false
+        }
+        )";
+  EXPECT_THAT(RequiredTest("AlphabeticallySortedMapBoolKeys")
+                  .ParseText(message(), kInput)
+                  .SerializeBinary(),
+              Yields(ParsedPayload(EqualsTextProto(kInput))));
+  EXPECT_THAT(RequiredTest("AlphabeticallySortedMapBoolKeys")
+                  .ParseText(message(), kInput)
+                  .SerializeText(),
+              Yields(ParsedPayload(EqualsTextProto(kInput))));
+}
+
+// The last-specified value will be retained in a parsed map.  Like the legacy
+// test, this only checks the binary output.
+TEST_P(TextMapTest, DuplicateMapKey) {
+  constexpr absl::string_view kInput = R"(
+        map_string_nested_message {
+          key: "duplicate"
+          value: { a: 123 }
+        }
+        map_string_nested_message {
+          key: "duplicate"
+          value: { corecursive: {} }
+        }
+        )";
+  constexpr absl::string_view kExpected = R"(
+        map_string_nested_message {
+          key: "duplicate"
+          value: { corecursive: {} }
+        }
+        )";
+  EXPECT_THAT(RequiredTest("DuplicateMapKey")
+                  .ParseText(message(), kInput)
+                  .SerializeBinary(),
+              Yields(ParsedPayload(EqualsTextProto(kExpected))));
+}
+
+INSTANTIATE_TEST_SUITE_P(All, TextMapTest, ValuesIn(Proto3TestMessageTypes()),
+                         MessageTypeParamName);
+
+}  // namespace
+}  // namespace conformance
+}  // namespace protobuf
+}  // namespace google

CodeLinaro-mirror/yocto-mirrors_github_protocolbuffers_protobuf

conformance/text_map_test.cc
@@ -1,0 +1,140 @@
+// Protocol Buffers - Google's data interchange format
+// Copyright 2025 Google LLC.  All rights reserved.
+//
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file or at
+// https://developers.google.com/open-source/licenses/bsd
+
+// Text-format conformance tests for map fields: entries given in key order
+// round-trip, and a duplicated key keeps the last value.  This replaces the
+// legacy map block of TextFormatConformanceTestSuiteImpl<M>::RunAllTests(),
+// which ran for the proto3-style message types only; the test names and the
+// requests sent to the testee are identical to the legacy ones.
+//
+// The legacy tests passed a prototype pre-filled with the map entries in
+// reverse order to RunValidTextFormatTestWithMessage(); that had no effect
+// (the legacy ConformanceRequestSetting only took the prototype's descriptor
+// and compared against prototype.New()), so nothing replaces it here.
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+#include "absl/strings/string_view.h"
+#include "conformance/binary_test_util.h"
+#include "conformance/matchers.h"
+#include "conformance/message_type_fixtures.h"
+#include "conformance/test_environment.h"
+
+namespace google {
+namespace protobuf {
+namespace conformance {
+namespace {
+
+using ::testing::ValuesIn;
+
+// The inputs below are the legacy raw string literals verbatim: their
+// surrounding whitespace and line breaks are part of the request bytes.
+
+using TextMapTest = MessageTypeConformanceTest;
+
+TEST_P(TextMapTest, AlphabeticallySortedMapStringKeys) {
+  constexpr absl::string_view kInput = R"(
+        map_string_string {
+          key: "a"
+          value: "value"
+        }
+        map_string_string {
+          key: "b"
+          value: "value"
+        }
+        map_string_string {
+          key: "c"
+          value: "value"
+        }
+        )";
+  EXPECT_THAT(Testee("AlphabeticallySortedMapStringKeys")
+                  .ParseText(message(), kInput)
+                  .SerializeBinary(),
+              Yields(ParsedPayload(EqualsTextProto(kInput))));
+  EXPECT_THAT(Testee("AlphabeticallySortedMapStringKeys")
+                  .ParseText(message(), kInput)
+                  .SerializeText(),
+              Yields(ParsedPayload(EqualsTextProto(kInput))));
+}
+
+TEST_P(TextMapTest, AlphabeticallySortedMapIntKeys) {
+  constexpr absl::string_view kInput = R"(
+        map_int32_int32 {
+          key: 1
+          value: 0
+        }
+        map_int32_int32 {
+          key: 2
+          value: 0
+        }
+        map_int32_int32 {
+          key: 3
+          value: 0
+        }
+        )";
+  EXPECT_THAT(Testee("AlphabeticallySortedMapIntKeys")
+                  .ParseText(message(), kInput)
+                  .SerializeBinary(),
+              Yields(ParsedPayload(EqualsTextProto(kInput))));
+  EXPECT_THAT(Testee("AlphabeticallySortedMapIntKeys")
+                  .ParseText(message(), kInput)
+                  .SerializeText(),
+              Yields(ParsedPayload(EqualsTextProto(kInput))));
+}
+
+TEST_P(TextMapTest, AlphabeticallySortedMapBoolKeys) {
+  constexpr absl::string_view kInput = R"(
+        map_bool_bool {
+          key: false
+          value: false
+        }
+        map_bool_bool {
+          key: true
+          value: false
+        }
+        )";
+  EXPECT_THAT(Testee("AlphabeticallySortedMapBoolKeys")
+                  .ParseText(message(), kInput)
+                  .SerializeBinary(),
+              Yields(ParsedPayload(EqualsTextProto(kInput))));
+  EXPECT_THAT(Testee("AlphabeticallySortedMapBoolKeys")
+                  .ParseText(message(), kInput)
+                  .SerializeText(),
+              Yields(ParsedPayload(EqualsTextProto(kInput))));
+}
+
+// The last-specified value will be retained in a parsed map.  Like the legacy
+// test, this only checks the binary output.
+TEST_P(TextMapTest, DuplicateMapKey) {
+  constexpr absl::string_view kInput = R"(
+        map_string_nested_message {
+          key: "duplicate"
+          value: { a: 123 }
+        }
+        map_string_nested_message {
+          key: "duplicate"
+          value: { corecursive: {} }
+        }
+        )";
+  constexpr absl::string_view kExpected = R"(
+        map_string_nested_message {
+          key: "duplicate"
+          value: { corecursive: {} }
+        }
+        )";
+  EXPECT_THAT(
+      Testee("DuplicateMapKey").ParseText(message(), kInput).SerializeBinary(),
+      Yields(ParsedPayload(EqualsTextProto(kExpected))));
+}
+
+INSTANTIATE_TEST_SUITE_P(All, TextMapTest, ValuesIn(Proto3TestMessageTypes()),
+                         MessageTypeParamName);
+
+}  // namespace
+}  // namespace conformance
+}  // namespace protobuf
+}  // namespace google